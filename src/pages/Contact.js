9035ec50eeeb88f81faeadb0db26644b0ebe7ee4
Violet Death <ladymariaownsbro@gmail.com>
2020-07-26 19:04:54 -0500

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -2,6 +2,14 @@ import React from 'react';
 
 export default function Contact(){
     return (
+        <div>
         <h1>Contact</h1>
+        <h3>-------------</h3>
+         <h2>email: ladymariaownsbro@gmail.com</h2>
+            
+         <h2>phone: 615-971-0152</h2>
+                  
+         <h2>discord: Violet#6776</h2>
+        </div>
     )
 }
