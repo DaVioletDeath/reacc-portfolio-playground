ae36f9d48119867db9ef4eed739a5850c1ed06f4
Violet Death <ladymariaownsbro@gmail.com>
2020-07-27 22:41:38 -0500

    D

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,6 +1,15 @@
 import React from 'react';
-export default function Project({title}){
+export default function Project({title, link, github, gif}){
     return (
-    <h1>Project: {title}</h1>
+        <div className="col-4 mt-3">
+        <div className="card">
+            <img src={gif} className="card-img-top" alt={title}/>
+            <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <a href= {link} className="btn text-light" style={{backgroundColor:'#8D07F6'}}>Deployed Site</a>
+            <a href={github} className="ml-3 btn text-light" style={{backgroundColor:'#8D07F6'}}>Github Repo</a>
+            </div>
+        </div>
+        </div>
     )
 }
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,15 +1,18 @@
 import React from 'react';
+import OnHand from '../assets/OnHand.png'
 import Project from "../components/Project"
 export default function Portfolio(){
     return (
-        <div>
+        <div className= "container-fluid">
             <h1>Portfolio</h1>
-            <Project title = 'OnHand' />
-            <Project title = 'First Impression' />
-            <Project title = 'PW About Me Page' />
-            <Project title = 'Employee Tracker App' />
-            <Project title = 'Day Planner' />
-            <Project title = 'Code Quiz' />
+            <div className='row'>
+            <Project title = 'OnHand' link = "https://msplawn.github.io/on-hand/" github = "https://github.com/msplawn/on-hand" gif = {OnHand} />
+            <Project title = 'First Impression' link = "https://msplawn.github.io/on-hand/" github = "https://github.com/msplawn/on-hand" gif = '' />
+            <Project title = 'PW About Me Page' link = "https://msplawn.github.io/on-hand/" github = "https://github.com/msplawn/on-hand" gif = '' />
+            <Project title = 'Employee Tracker App' link = "https://msplawn.github.io/on-hand/" github = "https://github.com/msplawn/on-hand" gif = '' />
+            <Project title = 'Day Planner' link = "https://msplawn.github.io/on-hand/" github = "https://github.com/msplawn/on-hand" gif = '' />
+            <Project title = 'Code Quiz' link = "https://msplawn.github.io/on-hand/" github = "https://github.com/msplawn/on-hand" gif = '' />
+            </div>
         </div>
     )
 }
