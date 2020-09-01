9058806f27dfe045491d3d4c471f3ca036077240
Violet Death <ladymariaownsbro@gmail.com>
2020-08-31 22:30:14 -0500

    Update AboutMe.js

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -6,8 +6,7 @@ export default function AboutMe(){
             <h1>About Me</h1>
             <p> I'm an up and coming programmer born in New Jersey and raised in Tennessee.
                 My mom was a software engineer for a government based firm and having been around programming and coding all my life, i started to become really interested in it and wanted
-                to learn more about it. So currently i am going through a coding boot camp hosted by Vanderbilt to learn more and while it's kicking my ass, I do feel like my knowledge is 
-                slowly growing and I am definitely becoming more and more interested in coding.
+                to learn more about it. I recently graduated from the full stack flex web development boot camp hosted by Vanderbilt University and am still honing my coding skills.
             </p>
         </div>
     )
