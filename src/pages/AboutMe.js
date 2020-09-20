ef82e253b474055c7c4b40d71408a42f00b4cb69
Violet Death <ladymariaownsbro@gmail.com>
2020-09-19 20:31:32 -0500

    Update AboutMe.js

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -5,7 +5,7 @@ export default function AboutMe(){
         <div className = "container">
             <h1>About Me</h1>
             <p> I'm an up and coming programmer born in New Jersey and raised in Tennessee.
-                My mom was a software engineer for a government based firm and having been around programming and coding all my life, i started to become really interested in it and wanted
+                My mom was a software engineer for a government based firm and having been around programming and coding all my life, I started to become really interested in it and wanted
                 to learn more about it. I recently graduated from the full stack flex web development boot camp hosted by Vanderbilt University and am still honing my coding skills.
             </p>
         </div>
